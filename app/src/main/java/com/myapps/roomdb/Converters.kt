00241e5fb1ab481d792b6d40aa713ac5596be90e
Praveen <pattarpraveen15@gmail.com>
2024-05-09 23:12:09 +0530

    Converters

---
+++ app/src/main/java/com/myapps/roomdb/Converters.kt
@@ -0,0 +1,18 @@
+package com.myapps.roomdb
+
+import androidx.room.TypeConverter
+import java.util.Date
+
+class Converters {
+
+    @TypeConverter
+    fun fromDateToLong(value :Date) :Long{
+        return value.time
+    }
+
+    @TypeConverter
+    fun fromLongToDate(value: Long) :Date{
+
+        return Date(value)
+    }
+}
--- app/src/main/java/com/myapps/roomdb/MainActivity.kt
+++ app/src/main/java/com/myapps/roomdb/MainActivity.kt
@@ -2,9 +2,13 @@ package com.myapps.roomdb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.lifecycle.Observer
 import androidx.room.Room
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import java.util.Date
 
 class MainActivity : AppCompatActivity() {
     lateinit var database: ContactDatabase
@@ -12,14 +16,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        database = Room.databaseBuilder(applicationContext,
-            ContactDatabase::class.java,
-            "contactDB").build()
+        database = ContactDatabase.getDatabase(this)
 
         GlobalScope.launch {
-            database.contactDao().insertContact(Contact(0,"John","99999"))
+            database.contactDao().insertContact(Contact(0,"John","99999", Date()))
         }
 
 
+    }
+
+    fun getData(view: View) {
+        database.contactDao().getContact().observe(this, Observer {
+            Log.d("Praveen",it.toString())
+        })
     }
 }
